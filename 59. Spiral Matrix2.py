c502e0b1b34d3fef1977814d86b282e97687b914
lpjjj1222 <694676764@qq.com>
2023-05-01 17:23:29 +0800

    大神解法

--- 59. Spiral Matrix2.py
+++ 59. Spiral Matrix2.py
@@ -6,10 +6,8 @@ class Solution:
 
         #使用[0] * 10会导致10个子列表共享同一个列表对象，所以修改一个子列表中的元素会影响其他子列表中对应位置的元素。
         #要避免这个问题，可以使用循环来逐个生成子列表
-        res = list()
-        for _ in range(n):
-            sublist = [0] * n
-            res.append(sublist)
+        res = [[0] * n for _ in range(n)]
+
 
         def right(row,column,num,end,small):
             if num > n*n:
@@ -65,3 +63,29 @@ class Solution:
 
 
         return right(0,0,1,n - 1,0)
+
+
+
+        #大神解法：
+        '''
+        def generateMatrix(self, n):
+            A = [[0] * n for _ in range(n)] #这一步一样，都是创建一个全是0的列表
+            i, j, di, dj = 0, 0, 0, 1   #i是行数 j是列数 
+                                        #di可以理解为下一步的行数变量or方向 dj同理 
+            for k in range(n*n):
+                A[i][j] = k + 1   #填入数字，k+1是因为range是每一个小1 总之填入的是k+1
+
+                if A[(i+di)%n][(j+dj)%n]:  #判断是否转弯 
+                                           #i+di 和 j+dj是下一个格子的坐标
+                                           # 行列数要%n是因为转弯有两种可能性
+                                           #一种是下一个格子已经被填过数了
+                                           #另一种是下一个格子超出界限了
+                                           #比如4*4的格子 填完4再向右的时候
+                                           #坐标就超出界限了
+                                           #这时用%n就可以把判断条件符合界限内
+
+                    di, dj = dj, -di     #这个改变方向的东西也很屌
+                i += di
+                j += dj
+            return A
+        '''
